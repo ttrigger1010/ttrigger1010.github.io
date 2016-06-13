d65a2987e1df804fe7b4ccbafa6266cca7d14003
ttrigger1010 <triggst10@gmail.com>
2016-06-13 15:49:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-var main = function() {
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: '0px'
-    }, 300);
+ var main = function() { 
+   $('.icon-menu').click(function() { 
+     $('.menu').animate({ 
+       left: '0px' 
+     }, 300); 
+ 
+ 
+     $('body').animate({ 
+       left: '285px' 
+     }, 300); 
+   }); 
+   
+    
+   $('.icon-close').click(function() { 
+       $('.menu').animate({ 
+           left: "-285px" 
+       }, 300); 
+        
+     $('body').animate({ 
+         left: "0px" 
+     }, 300); 
+   }); 
+    
+   $('.menu li.dropdown').ready(function() { 
+     $( '.dropdown' ).hover( 
+         function(){ 
+             $(this).children('.sub-menu').slideDown(200); 
+         }, 
+         function(){ 
+             $(this).children('.sub-menu').slideUp(200); 
+         } 
+     ); 
+    
+ }; 
+ };
+$(document).ready(main); 
 
-    $('body').animate({
-      left: '285px'
-    }, 300);
-  });
-  
-  
-  $('.icon-close').click(function() {
-      $('.menu').animate({
-          left: "-285px"
-      }, 300);
-      
-    $('body').animate({
-        left: "0px"
-    }, 300);
-  });
-  
-  /* $('.menu li.dropdown').ready(function() { 
-    $( '.dropdown' ).hover(
-        function(){
-            $(this).children('.sub-menu').slideDown(201);
-        },
-        function(){
-            $(this).children('.sub-menu').slideUp(200);
-        }
-    );
-  };
-  */
-};
-$(document).ready(main);
